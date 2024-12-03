6f64478bb7788b0dd565e18f25863f421fe15142
antbern <40672068+antbern@users.noreply.github.com>
2024-12-03 21:55:00 +0100

    add Header with metadata to the binary xyz file

--- src/io/xyz.rs
+++ src/io/xyz.rs
@@ -25,16 +25,86 @@ pub struct XyzRecord {
 
 pub struct XyzInternalWriter<W: Write + Seek> {
     inner: Option<W>,
-    records_written: u64,
+    header: Header,
     // for stats
     start: Option<Instant>,
+}
+
+/// File header containing information about the data, such as number of records and the min/max in
+/// each dimension.
+#[derive(Debug, PartialEq)]
+pub struct Header {
+    n_records: u64,
+    min_x: f64,
+    max_x: f64,
+    min_y: f64,
+    max_y: f64,
+    min_z: f64,
+    max_z: f64,
+}
+
+impl FromToBytes for Header {
+    fn from_bytes<R: std::io::Read>(reader: &mut R) -> std::io::Result<Self> {
+        let n_records = u64::from_bytes(reader)?;
+        let min_x = f64::from_bytes(reader)?;
+        let max_x = f64::from_bytes(reader)?;
+        let min_y = f64::from_bytes(reader)?;
+        let max_y = f64::from_bytes(reader)?;
+        let min_z = f64::from_bytes(reader)?;
+        let max_z = f64::from_bytes(reader)?;
+        Ok(Self {
+            n_records,
+            min_x,
+            max_x,
+            min_y,
+            max_y,
+            min_z,
+            max_z,
+        })
+    }
+
+    fn to_bytes<W: std::io::Write>(&self, writer: &mut W) -> std::io::Result<()> {
+        self.n_records.to_bytes(writer)?;
+        self.min_x.to_bytes(writer)?;
+        self.max_x.to_bytes(writer)?;
+        self.min_y.to_bytes(writer)?;
+        self.max_y.to_bytes(writer)?;
+        self.min_z.to_bytes(writer)?;
+        self.max_z.to_bytes(writer)
+    }
+}
+
+impl Header {
+    /// Creates a new header with all values set to the extremes.
+    fn new() -> Self {
+        Self {
+            n_records: 0,
+            min_x: f64::INFINITY,
+            max_x: f64::NEG_INFINITY,
+            min_y: f64::INFINITY,
+            max_y: f64::NEG_INFINITY,
+            min_z: f64::INFINITY,
+            max_z: f64::NEG_INFINITY,
+        }
+    }
+
+    /// Updates the header with the values from the given record.
+    fn update(&mut self, record: &XyzRecord) {
+        self.n_records += 1;
+        self.min_x = self.min_x.min(record.x);
+        self.max_x = self.max_x.max(record.x);
+        self.min_y = self.min_y.min(record.y);
+        self.max_y = self.max_y.max(record.y);
+        self.min_z = self.min_z.min(record.z as f64);
+        self.max_z = self.max_z.max(record.z as f64);
+    }
 }
 
 impl<W: Write + Seek> XyzInternalWriter<W> {
     pub fn new(inner: W) -> Self {
         Self {
             inner: Some(inner),
-            records_written: 0,
+            header: Header::new(),
             start: None,
         }
     }
@@ -50,18 +120,22 @@ impl<W: Write + Seek> XyzInternalWriter<W> {
         }
 
         // write the header (format + length) on the first write
-        if self.records_written == 0 {
+        if self.header.n_records == 0 {
             self.start = Some(Instant::now());
 
             inner.write_all(XYZ_MAGIC)?;
-            // Write the temporary number of records as all FF
-            u64::MAX.to_bytes(inner)?;
+            // Write the header to reserve space for it
+            self.header.to_bytes(inner)?;
         }
 
+        // update the header with the new records
+        for r in records {
+            self.header.update(r);
+        }
         let bytes: &[u8] = bytemuck::cast_slice(records);
         inner.write_all(bytes)?;
 
-        self.records_written += records.len() as u64;
+        self.header.n_records += records.len() as u64;
         Ok(())
     }
 
@@ -73,18 +147,18 @@ impl<W: Write + Seek> XyzInternalWriter<W> {
 
         // seek to the beginning of the file and write the number of records
         inner.seek(std::io::SeekFrom::Start(XYZ_MAGIC.len() as u64))?;
-        self.records_written.to_bytes(&mut inner)?;
+        self.header.to_bytes(&mut inner)?;
 
         // log statistics about the written records
         if let Some(start) = self.start {
             let elapsed = start.elapsed();
             debug!(
                 "Wrote {} records in {:.2?} ({:.2?}/record, {:.3}M records/s, {:.2}MB/s)",
-                self.records_written,
+                self.header.n_records,
                 elapsed,
-                elapsed / self.records_written as u32,
-                self.records_written as f64 / (10e6 * elapsed.as_secs_f64()),
-                self.records_written as f64 * size_of::<XyzRecord>() as f64
+                elapsed / self.header.n_records as u32,
+                self.header.n_records as f64 / (10e6 * elapsed.as_secs_f64()),
+                self.header.n_records as f64 * size_of::<XyzRecord>() as f64
                     / (1024.0 * 1024.0 * elapsed.as_secs_f64()),
             );
         }
@@ -102,7 +176,7 @@ impl<W: Write + Seek> Drop for XyzInternalWriter<W> {
 
 pub struct XyzInternalReader<R: Read> {
     inner: R,
-    n_records: u64,
+    header: Header,
     records_read: u64,
     // for stats
     start: Option<Instant>,
@@ -121,11 +195,11 @@ impl<R: Read> XyzInternalReader<R> {
             ));
         }
 
-        // read the number of records, defined by the first u64
-        let n_records = u64::from_bytes(&mut inner)?;
+        // read the header containing the metadata
+        let header = Header::from_bytes(&mut inner)?;
         Ok(Self {
             inner,
-            n_records,
+            header,
             records_read: 0,
             start: None,
             buffer: [XyzRecord::default(); 1024],
@@ -133,8 +207,8 @@ impl<R: Read> XyzInternalReader<R> {
     }
 
     pub fn next_chunk(&mut self) -> std::io::Result<Option<&[XyzRecord]>> {
-        if self.records_read >= self.n_records {
-            // TODO: log statistics about the read records
+        if self.records_read >= self.header.n_records {
+            // log statistics about the read records
             if let Some(start) = self.start {
                 let elapsed = start.elapsed();
                 debug!(
@@ -156,7 +230,7 @@ impl<R: Read> XyzInternalReader<R> {
         }
 
         // read as many as we can fit in the buffer
-        let records_left = self.n_records - self.records_read;
+        let records_left = self.header.n_records - self.records_read;
         let records_to_read = (self.buffer.len() as u64).min(records_left);
 
         // treat buffer as mutable slice of bytes
@@ -168,6 +242,10 @@ impl<R: Read> XyzInternalReader<R> {
         // return reference to it
         Ok(Some(records_buffer))
     }
+
+    pub fn header(&self) -> &Header {
+        &self.header
+    }
 }
 
 #[cfg(test)]
@@ -177,6 +255,43 @@ mod test {
     use crate::io::xyz::XyzRecord;
 
     use super::*;
+
+    #[test]
+    fn test_header() {
+        let mut header = Header::new();
+        header.update(&XyzRecord {
+            x: 1.0,
+            y: 2.0,
+            z: 3.0,
+            classification: 4,
+            number_of_returns: 5,
+            return_number: 6,
+            _padding: 0,
+        });
+        header.update(&XyzRecord {
+            x: -12.0,
+            y: -3.0,
+            z: 40.0,
+            classification: 5,
+            number_of_returns: 6,
+            return_number: 7,
+            _padding: 0,
+        });
+
+        assert_eq!(header.n_records, 2);
+        assert_eq!(header.min_x, -12.0);
+        assert_eq!(header.max_x, 1.0);
+        assert_eq!(header.min_y, -3.0);
+        assert_eq!(header.max_y, 2.0);
+        assert_eq!(header.min_z, 3.0);
+        assert_eq!(header.max_z, 40.0);
+
+        let mut buff = Vec::new();
+        header.to_bytes(&mut buff).unwrap();
+        let read_header = Header::from_bytes(&mut buff.as_slice()).unwrap();
+
+        assert_eq!(header, read_header);
+    }
 
     #[test]
     fn test_writer_reader_many() {
